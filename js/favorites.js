09ff56eec73460188870e006f4e43382778912f0
William Milanez <william_milanez@hotmail.com>
2025-01-22 19:55:51 -0300

    métodos estáticos e promessas (api / json)

--- js/favorites.js
+++ js/favorites.js
@@ -1,3 +1,29 @@
+export class GithubUser {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}`
+
+    // forma desestruturada
+        return fetch(endpoint)
+        .then(data => data.json())
+        .then(({ login, name, public_repos, followers }) => ({
+            login,
+            name,
+            public_repos,
+            followers
+        }))
+
+    // forma estruturada
+        // return fetch(endpoint)
+        // .then(data => data.json())
+        // .then(data => ({
+        //     login: data.login,
+        //     name: data.name,
+        //     public_repos: data.public_repos,
+        //     followers: data.followers
+        // }))
+    }
+}
+
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
 export class Favorites {
